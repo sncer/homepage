70a88d882aeef2fe28bb140536c291184e27d6f9
sncer <chaobin.sncer@gmail.com>
2016-06-15 16:13:27 +0800

    First commit
    First commit

---
+++ js/custom.js
@@ -0,0 +1,54 @@
+(function ($) {
+
+    jQuery(window).load(function () {
+        jQuery("#preloader").delay(1000).fadeOut("slow");
+        jQuery("#load").delay(1000).fadeOut("slow");
+    });
+
+
+    //jQuery to collapse the navbar on scroll
+    $(window).scroll(function () {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+    });
+
+    //jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function () {
+        $('.navbar-nav li a').bind('click', function (event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+        $('.page-scroll a').bind('click', function (event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+        
+    });
+    
+    
+
+})(jQuery);
+
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+document.getElementById("year").innerHTML = getAge("1991/08/29");
+document.getElementById("fullyear").innerHTML = new Date().getFullYear();
+
+
